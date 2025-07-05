f277c252f6b7bfabd824f477a8e19896618b4fd9
Gama Prayogo <ogi.prayogo@gmail.com>
2025-07-05 14:17:05 +0700

    Fixed install.sh and add alias

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
 
-# Create symlinks
+# create symlinks in $HOME for all files in the zsh and git directories
 for dir in zsh git; do
-    for file in "$dir"/*; do
+    for file in "$dir"/.*; do
         if [ -f "$file" ]; then
             filename=$(basename "$file")
             ln -sf "$(pwd)/$file" "$HOME/.$filename"
-            echo "Created symlink for $filename"
+            # echo "Created symlink for $filename"
         fi
     done
 done
